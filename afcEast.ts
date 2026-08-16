cbdc228159eb0c2912ae02ba4cc9dd5d0f1ad794
kmdbb9zg5k-png <kmdbb9zg5k@privaterelay.appleid.com>
2026-08-16 18:40:52 -0400

    Update afcEast.ts

--- afcEast.ts
+++ afcEast.ts
@@ -1,4 +1,4 @@
-import { Player } from '../../types';
+import { Player } from './types';
 
 export const AFC_EAST_PLAYERS: Player[] = [
   // ===================== BUFFALO BILLS (BUF) =====================
