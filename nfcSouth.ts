e20f95227af24f0a5b2b1e6eb9a1227c7dc16305
kmdbb9zg5k-png <kmdbb9zg5k@privaterelay.appleid.com>
2026-08-16 18:42:22 -0400

    Update nfcSouth.ts

--- nfcSouth.ts
+++ nfcSouth.ts
@@ -1,4 +1,4 @@
-import { Player } from '../../types';
+import { Player } from './types';
 
 export const NFC_SOUTH_PLAYERS: Player[] = [
   // ===================== ATLANTA FALCONS (ATL) =====================
