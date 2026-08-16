644fae6497e5fb43149ef54b1b95348d6cf6c71d
kmdbb9zg5k-png <kmdbb9zg5k@privaterelay.appleid.com>
2026-08-16 18:42:09 -0400

    Update nfcNorth.ts

--- nfcNorth.ts
+++ nfcNorth.ts
@@ -1,4 +1,4 @@
-import { Player } from '../../types';
+import { Player } from './types';
 
 export const NFC_NORTH_PLAYERS: Player[] = [
   // ===================== CHICAGO BEARS (CHI) =====================
