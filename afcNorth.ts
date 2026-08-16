d35fa297f7ce0606d661eff7dcdd0f52a6e9b816
kmdbb9zg5k-png <kmdbb9zg5k@privaterelay.appleid.com>
2026-08-16 18:41:06 -0400

    Update afcNorth.ts

--- afcNorth.ts
+++ afcNorth.ts
@@ -1,4 +1,4 @@
-import { Player } from '../../types';
+import { Player } from './types';
 
 export const AFC_NORTH_PLAYERS: Player[] = [
   // ===================== BALTIMORE RAVENS (BAL) =====================
