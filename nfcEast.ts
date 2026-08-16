9eafe6a4348c2c563595712229fb62d2445350ad
kmdbb9zg5k-png <kmdbb9zg5k@privaterelay.appleid.com>
2026-08-16 18:41:57 -0400

    Update nfcEast.ts

--- nfcEast.ts
+++ nfcEast.ts
@@ -1,4 +1,4 @@
-import { Player } from '../../types';
+import { Player } from './types';
 
 export const NFC_EAST_PLAYERS: Player[] = [
   // ===================== DALLAS COWBOYS (DAL) =====================
