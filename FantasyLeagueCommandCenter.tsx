695fc3709073475440cf16b9b9c09d3a15bd4a39
kmdbb9zg5k-png <kmdbb9zg5k@privaterelay.appleid.com>
2026-08-23 10:46:40 -0400

    Use ballknower.com in FantasyLeagueCommandCenter.tsx

--- FantasyLeagueCommandCenter.tsx
+++ FantasyLeagueCommandCenter.tsx
@@ -23,7 +23,7 @@ interface Props {
   onGoToSimulation: () => void;
 }
 
-const PUBLIC_APP_ORIGIN = 'https://ballknowerofficial.com';
+const PUBLIC_APP_ORIGIN = 'https://ballknower.com';
 
 const relativeTime = (iso:string) => {
   const seconds=Math.max(0,Math.floor((Date.now()-new Date(iso).getTime())/1000));
